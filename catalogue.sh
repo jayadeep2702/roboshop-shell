46935026b1bf20e597c88b4b3b41a338c9548b45
akhil <akhil@gmail.com>
2023-06-26 13:23:03 +0530

    terraform update

--- catalogue.sh
+++ catalogue.sh
@@ -9,6 +9,7 @@ echo -e "\e[32m >>>>>>>add application user<<<<<<<\e]0m"
 useradd roboshop
 
 echo -e "\e[32m >>>>>>>crating application directory<<<<<<<\e]0m"
+rm -rf /app
 mkdir /app
 
 echo -e "\e[32m >>>>>>>downloading App content<<<<<<<\e]0m"
@@ -22,7 +23,7 @@ echo -e "\e[32m >>>>>>>downloading dependencies<<<<<<<\e]0m"
 npm install
 
 echo -e "\e[32m >>>>>>>copy catalogue.service file<<<<<<<\e]0m"
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp /home/centos/roboshop-shellcatalogue.service /etc/systemd/system/catalogue.service
 
 echo -e "\e[32m >>>>>>>start catalogue service<<<<<<<\e]0m"
 systemctl daemon-reload
