4c1a12fde9b25f32c0857b6a1ad36c8dd6af6ab3
akhil <akhil@gmail.com>
2023-06-26 19:34:30 +0530

    terraform update

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,11 @@
+echo -e "\e[32m >>>>>>>Disable mysql 8 version<<<<<<<\e]0m"
 yum module disable mysql -y
+echo -e "\e[32m >>>>>>>configuring mysql repo<<<<<<<\e]0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo
+echo -e "\e[32m >>>>>>>installing mysql server<<<<<<<\e]0m"
 yum install mysql-community-server -y
+echo -e "\e[32m >>>>>>>start mysql service<<<<<<<\e]0m"
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
+echo -e "\e[32m >>>>>>>update password for mysql<<<<<<<\e]0m"
 mysql_secure_installation --set-root-pass RoboShop@1
