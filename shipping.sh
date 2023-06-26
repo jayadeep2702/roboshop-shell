3bed7015a9829ac3750f641e9e6252d874e1e108
akhil <akhil@gmail.com>
2023-06-26 19:43:41 +0530

    terraform update

--- shipping.sh
+++ shipping.sh
@@ -1,16 +1,26 @@
+echo -e "\e[32m >>>>>>>install Maven<<<<<<<\e]0m"
 yum install maven -y
+echo -e "\e[32m >>>>>>>add user for application<<<<<<<\e]0m"
 useradd roboshop
+echo -e "\e[32m >>>>>>>create directory<<<<<<<\e]0m"
 mkdir /app
+echo -e "\e[32m >>>>>>>download application content<<<<<<<\e]0m"
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
 cd /app
+echo -e "\e[32m >>>>>>>unzip app content<<<<<<<\e]0m"
 unzip /tmp/shipping.zip
 cd /app
+echo -e "\e[32m >>>>>>>download dependencies<<<<<<<\e]0m"
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
+echo -e "\e[32m >>>>>>>copy shipping service file<<<<<<<\e]0m"
 cp shipping.service /etc/systemd/system/shipping.service
+
+echo -e "\e[32m >>>>>>>install mysql<<<<<<<\e]0m"
+yum install mysql -y
+echo -e "\e[32m >>>>>>>update password for mysql<<<<<<<\e]0m"
+mysql -h mysql-dev.jdevops72.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
+echo -e "\e[32m >>>>>>>start shipping service<<<<<<<\e]0m"
 systemctl daemon-reload
 systemctl enable shipping
-systemctl start shipping
-yum install mysql -y
-mysql -h mysql-dev.jdevops72.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
 systemctl restart shipping
