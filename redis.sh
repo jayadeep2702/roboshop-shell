cdd7718c968f3bd55f4a2c6db9703f0779f291ca
akhil <akhil@gmail.com>
2023-06-26 15:39:27 +0530

    terraform update

---
+++ redis.sh
@@ -0,0 +1,10 @@
+echo -e "\e[32m>>>>>>>installing redis repo<<<<<<<\e]0m"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+echo -e "\e[32m>>>>>>install redis<<<<<<<\e]0m"
+yum module enable redis:remi-6.2 -y
+yum install redis -y
+echo -e "\e[32m>>>>>>>update redis listen ip adress<<<<<<<\e]0m"
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
+echo -e "\e[32m>>>>>>>start redis service<<<<<<<\e]0m"
+systemctl enable redis
+systemctl start redis
