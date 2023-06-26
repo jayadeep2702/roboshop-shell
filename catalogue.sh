3ebf0876e5085c2796632cd4f3b7bdc11d6f782b
akhil <akhil@gmail.com>
2023-06-26 13:27:50 +0530

    terraform update

--- catalogue.sh
+++ catalogue.sh
@@ -37,4 +37,4 @@ echo -e "\e[32m >>>>>>>Installing mongodb<<<<<<<\e]0m"
 yum install mongodb-org-shell -y
 
 echo -e "\e[32m >>>>>>>load schema for mongodb<<<<<<<\e]0m"
-mongo --host mongo-dev.jdevops72.online </app/schema/catalogue.js
+mongo --host mongodb-dev.jdevops72.online </app/schema/catalogue.js
