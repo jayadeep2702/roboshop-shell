3435d7c2e0b977b2e3a04713dfdac7ff7c93403c
akhil <akhil@gmail.com>
2023-06-26 20:01:25 +0530

    terraform update

--- payment.sh
+++ payment.sh
@@ -1,12 +1,20 @@
+echo -e "\e[32m >>>>>>>install python<<<<<<<\e]0m"
 yum install python36 gcc python3-devel -y
+echo -e "\e[32m >>>>>>>add user for the app<<<<<<<\e]0m"
 useradd roboshop
+echo -e "\e[32m >>>>>>>create a directory<<<<<<<\e]0m"
 mkdir /app
+echo -e "\e[32m >>>>>>>download app content<<<<<<<\e]0m"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
 cd /app
+echo -e "\e[32m >>>>>>>unzip app content<<<<<<<\e]0m"
 unzip /tmp/payment.zip
 cd /app
+echo -e "\e[32m >>>>>>>download dependencies<<<<<<<\e]0m"
 pip3.6 install -r requirements.txt
-cp payment.service /etc/yum.repos.d/payment.service
+echo -e "\e[32m >>>>>>>copy payment service file<<<<<<<\e]0m"
+cp /home/centos/roboshop-shell/payment.service /etc/yum.repos.d/payment.service
+echo -e "\e[32m >>>>>>>start payment service<<<<<<<\e]0m"
 systemctl daemon-reload
 systemctl enable payment
 systemctl start payment
