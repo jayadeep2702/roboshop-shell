0e17ca1113521435b578938113b561c182ce6b7c
akhil <akhil@gmail.com>
2023-07-11 23:44:43 +0530

    moving files

--- cart.sh
+++ cart.sh
@@ -2,33 +2,7 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-echo -e "\e[32m>>>>>>>configure node js repo<<<<<<<\e]0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+component=cart
 
-echo -e "\e[32m >>>>>>>installing Nodejs<<<<<<<\e]0m"
-yum install nodejs -y
-
-
-echo -e "\e[32m >>>>>>>add application user<<<<<<<\e]0m"
-useradd ${app_user}
-
-echo -e "\e[32m >>>>>>>crating application directory<<<<<<<\e]0m"
-rm -rf /app
-mkdir /app
-
-echo -e "\e[32m >>>>>>>downloading App content<<<<<<<\e]0m"
-curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
-cd /app
-
-echo -e "\e[32m >>>>>>>unzip app content<<<<<<<\e]0m"
-unzip /tmp/cart.zip
-
-echo -e "\e[32m >>>>>>>downloading dependencies<<<<<<<\e]0m"
-npm install
-echo -e "\e[32m >>>>>>>copying cart service file<<<<<<<\e]0m"
-cp ${script_path}/cart.service /etc/systemd/system/cart.service
-echo -e "\e[32m >>>>>>>service cart start<<<<<<<\e]0m"
-systemctl daemon-reload
-systemctl enable cart
-systemctl start cart
+function_nodejs
 
--- common.sh
+++ common.sh
@@ -1,1 +1,34 @@
 app_user=roboshop
+
+function_nodejs() {
+  echo -e "\e[32m>>>>>>>configure node js repo<<<<<<<\e]0m"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+  echo -e "\e[32m >>>>>>>installing Nodejs<<<<<<<\e]0m"
+  yum install nodejs -y
+
+
+  echo -e "\e[32m >>>>>>>add application user<<<<<<<\e]0m"
+  useradd ${app_user}
+
+  echo -e "\e[32m >>>>>>>crating application directory<<<<<<<\e]0m"
+  rm -rf /app
+  mkdir /app
+
+  echo -e "\e[32m >>>>>>>downloading App content<<<<<<<\e]0m"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+  cd /app
+
+  echo -e "\e[32m >>>>>>>unzip app content<<<<<<<\e]0m"
+  unzip /tmp/${component}.zip
+
+  echo -e "\e[32m >>>>>>>downloading dependencies<<<<<<<\e]0m"
+  npm install
+  echo -e "\e[32m >>>>>>>copying cart service file<<<<<<<\e]0m"
+  cp ${script_path}/cart.service /etc/systemd/system/${component}.service
+  echo -e "\e[32m >>>>>>>service cart start<<<<<<<\e]0m"
+  systemctl daemon-reload
+  systemctl enable ${component}
+  systemctl start ${component}
+
+}
