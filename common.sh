25e2582be7b89445dd1517c7891600fd975e11a7
akhil <akhil@gmail.com>
2023-07-13 16:35:12 +0530

    moving files

--- common.sh
+++ common.sh
@@ -1,8 +1,19 @@
 app_user=roboshop
 
+
 print_head() {
   echo -e "\e[35m>>>>>>>$1<<<<<<<\e]0m"
 }
+  if [ "$schema_setup" == "mongo" ]; then
+    print_head "copy mongo repo"
+    cp ${script_path}mongo.repo /etc/yum.repos.d/mongo.repo
+    print_head "installing mongodb client"
+    yum install mongodb-org-shell -y
+
+    print_head load mongodb schema"
+    mongo --host mongodb-dev.jdevops72.online </app/schema/${component}.js
+  fi
+
 function_nodejs() {
   print_head "configure node js repo"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
@@ -34,4 +45,5 @@ function_nodejs() {
   systemctl enable ${component}
   systemctl start ${component}
 
+}
 }
--- user.sh
+++ user.sh
@@ -4,12 +4,6 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
 component=user
+schema_setup=mongo
 function_nodejs
 
-echo -e "\e[32m >>>>>>>copy mongo repo<<<<<<<\e]0m"
-cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
-echo -e "\e[32m >>>>>>>installing mongodb client<<<<<<<\e]0m"
-yum install mongodb-org-shell -y
-
-echo -e "\e[32m >>>>>>>load mongodb schema<<<<<<<\e]0m"
-mongo --host mongodb-dev.jdevops72.online </app/schema/user.js
