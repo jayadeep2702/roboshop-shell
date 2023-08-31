33a17dbb9717f1b495b541a6f74bbbb31afdbce3
akhil <akhil@gmail.com>
2023-08-31 23:40:09 +0530

    moving files

--- payment.sh
+++ payment.sh
@@ -8,25 +8,5 @@ then
 echo Roboshop Appuser passwd is missing
 fi
 
-echo -e "\e[32m >>>>>>>install python<<<<<<<\e]0m"
-yum install python36 gcc python3-devel -y
-echo -e "\e[32m >>>>>>>add user for the app<<<<<<<\e]0m"
-useradd ${app_user}
-echo -e "\e[32m >>>>>>>create a directory<<<<<<<\e]0m"
-rm -rf /app
-mkdir /app
-echo -e "\e[32m >>>>>>>download app content<<<<<<<\e]0m"
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
-cd /app
-echo -e "\e[32m >>>>>>>unzip app content<<<<<<<\e]0m"
-unzip /tmp/payment.zip
-cd /app
-echo -e "\e[32m >>>>>>>download dependencies<<<<<<<\e]0m"
-pip3.6 install -r requirements.txt
-echo -e "\e[32m >>>>>>>copy payment service file<<<<<<<\e]0m"
-sed -i -e "s|rabbitmq_user_passwd|${rabbitmq_user_passwd}|" ${script_path}/payment.service
-cp ${script_path}/payment.service /etc/systemd/system/payment.service
-echo -e "\e[32m >>>>>>>start payment service<<<<<<<\e]0m"
-systemctl daemon-reload
-systemctl enable payment
-systemctl restart payment
+component=payment
+func_python
