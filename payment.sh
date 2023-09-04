896050a5117b8871fdbd93addaefd67ab7dae833
akhil <akhil@gmail.com>
2023-09-04 21:55:56 +0530

    moving files

--- payment.sh
+++ payment.sh
@@ -1,9 +1,9 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-rabbitmq_user_password=$1
+rabbitmq_user_passwd=$1
 
-if [ -z "$rabbitmq_user_password" ]; then
+if [ -z "$rabbitmq_user_passwd" ]; then
   echo Input Roboshop Appuser Password Missing
   exit 1
 fi
